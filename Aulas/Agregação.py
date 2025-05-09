a1e69e7a133f6246a3a7baa881b55dd739647fc4
Vargas <iago.vargas@ufn.edu.br>
2025-05-08 23:13:18 -0300

    Teste

---
+++ Aulas/Agregação.py
@@ -0,0 +1,30 @@
+#Agregação é uma forma mais especializada em associar
+#Um objeto possui muitos objetos
+
+class Carrinho:
+    def __init__(self):
+        self._produtos = []
+
+    def total(self):
+        return sum([p.preco for p in self._produtos])
+
+    def inserir_produtos(self, *produtos):
+        for produto in produtos:
+            self._produtos.append(produto)
+
+    def listar_produtos(self):
+        print()
+        for produto in self._produtos:
+            print(produto.nome, produto.preco)
+        print()
+
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+
+carrinho = Carrinho()
+p1, p2 = Produto("Caneta", 1.20), Produto("Borracha", 20.00)
+carrinho.inserir_produtos(p1, p2)
+carrinho.listar_produtos()
+print(carrinho.total())
